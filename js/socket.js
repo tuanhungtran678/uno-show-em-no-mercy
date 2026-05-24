100e2f66d21b2b23861c1e39361d53af2880e0df
tuanhungtran678 <tuanhungtran678@gmail.com>
2026-05-24 18:49:37 +0700

    Update socket.js and server.js

--- js/socket.js
+++ js/socket.js
@@ -5,3 +5,24 @@ socket.on("connect", () => {
     console.log("Connected!");
 
 });
+socket.on("roomCreated", roomCode => {
+
+    alert(
+        "Room Code: " + roomCode
+    );
+
+});
+
+socket.on("gameStart", roomCode => {
+
+    alert(
+        "Game Started!"
+    );
+
+});
+
+socket.on("errorMessage", message => {
+
+    alert(message);
+
+});
--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const express = require("express");
+
 const http = require("http");
+
 const { Server } = require("socket.io");
-const socket = io("http://localhost:3000");
 
 const app = express();
 
@@ -13,28 +14,104 @@ const io = new Server(server, {
     }
 });
 
-let players = [];
+const rooms = {};
+
+function generateRoomCode() {
+
+    return Math.random()
+        .toString(36)
+        .substring(2, 8)
+        .toUpperCase();
+
+}
 
 io.on("connection", socket => {
 
-    console.log("Player joined");
+    console.log(
+        "Player connected:",
+        socket.id
+    );
 
-    players.push(socket.id);
+    socket.on("createRoom", () => {
 
-    socket.emit("playerIndex", players.length - 1);
+        const roomCode =
+            generateRoomCode();
 
-    socket.on("playCard", data => {
+        rooms[roomCode] = {
+            players: [socket.id]
+        };
 
-        socket.broadcast.emit("opponentPlayed", data);
+        socket.join(roomCode);
+
+        socket.emit(
+            "roomCreated",
+            roomCode
+        );
+
+        console.log(
+            "Room created:",
+            roomCode
+        );
+
+    });
+
+    socket.on("joinRoom", roomCode => {
+
+        const room = rooms[roomCode];
+
+        if (!room) {
+
+            socket.emit(
+                "errorMessage",
+                "Room not found"
+            );
+
+            return;
+
+        }
+
+        if (room.players.length >= 2) {
+
+            socket.emit(
+                "errorMessage",
+                "Room full"
+            );
+
+            return;
+
+        }
+
+        room.players.push(socket.id);
+
+        socket.join(roomCode);
+
+        io.to(roomCode).emit(
+            "gameStart",
+            roomCode
+        );
+
+        console.log(
+            "Player joined room:",
+            roomCode
+        );
 
     });
 
     socket.on("disconnect", () => {
 
-        players = players.filter(id => id !== socket.id);
+        console.log(
+            "Disconnected:",
+            socket.id
+        );
 
     });
 
 });
 
-server.listen(3000);
+server.listen(3000, () => {
+
+    console.log(
+        "Server running on port 3000"
+    );
+
+});
